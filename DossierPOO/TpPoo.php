7a96198a037494b3fa4f10394164ad53687b9ce8
Cheikh Mbacké <lionnelfatt@192.168.1.33>
2026-04-26 13:09:35 +0000

    class Utilisateur

--- DossierPOO/TpPoo.php
+++ DossierPOO/TpPoo.php
@@ -2,7 +2,7 @@
 
 // Pour commit: commit mokoy save sama mac push diokh ko git
 // git add .
-// git commit -m "linga modifier" 
+// git commit -m "linga modifier" git commit -m "class Utilisateur" 
 // git push
 
 interface Authentifiable {
@@ -48,4 +48,33 @@ class Personne {
     public function afficherInfos() {
         echo "ID: " . $this->id . "  Nom: " . $this->nom . "  Email: " . $this->email . "\n";
     }
+}
+
+abstract class Utilisateur extends Personne implements Authentifiable, Affichable {
+    private $login;
+    private $motDePasse;
+    private static $nombreUtilisateurs = 0;
+
+    public function __construct($id, $nom, $email, $login, $motDePasse) {
+        parent::__construct($id, $nom, $email);
+        $this->login = $login;
+        $this->motDePasse = $motDePasse;
+        self::$nombreUtilisateurs++;
+    }
+
+    public function getLogin() { return $this->login; }
+    public function getMotDePasse() { return $this->motDePasse; }
+
+    public function setLogin($login) { $this->login = $login; }
+    public function setMotDePasse($motDePasse) { $this->motDePasse = $motDePasse; }
+
+    public function seConnecter() {
+        echo "Utilisateur " . $this->getLogin() . " connecté.\n";
+    }
+
+    public static function afficherNombre() {
+        echo "Nombre d'utilisateurs : " . self::$nombreUtilisateurs . "\n";
+    }
+
+    abstract public function afficherRole();
 }
